28c6bf587d0382686193c9b5767c00e2255f4d7b
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2021-06-09 11:15:14 +0200

    Add database connection example

--- README.md
+++ README.md
@@ -8,7 +8,16 @@ If you find it useful, enjoy!
 
 ## Environment variables
 
-* ENVIRONMENT contain the environment name that is simply shown in the root
+* `ENVIRONMENT` contain the environment name that is simply shown in the root
   page of the application
+* `COLOR` is used as the color of the text
+* `DATABASE_URL` is the connection string to a PostgreSQL database
+* `QUERY` is the SQL query used
+* `LISTEN_ADDRESS` is the endpoint where we should listen for HTTP requests
 
-* COLOR is used as the color of the text
+## Endpoints
+
+* `/` show a simple page with the desired color and environment
+* `/.well-known/check` check if we can connect to PostgreSQL
+* `/tx` execute the query in `QUERY`
+* `/metrics` Prometheus metrics endpoint
--- cmd/minimal-app/main.go
+++ cmd/minimal-app/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	_ "github.com/lib/pq"
 )
 
 var (
 	color       string
 	environment string
+	query       string
+	db          *sql.DB
 )
 
 var (
@@ -23,13 +29,19 @@ var (
 		Name: "minimal_app_requests",
 		Help: "The total number of processed requests",
 	})
+
+	txProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "minimal_app_tx",
+		Help: "The total number of processed transactions",
+	})
+
+	errorsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "minimal_app_errors",
+		Help: "The total number of errors",
+	})
 )
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
-	if r.RequestURI != "/" {
-		return
-	}
-
 	w.WriteHeader(http.StatusOK)
 	_, _ = fmt.Fprintf(w, "<p style='color: %v; text-weight: 2000'>Environment: %v</p>",
 		color,
@@ -39,17 +51,60 @@ func rootPage(w http.ResponseWriter, r *http.Request) {
 	opsProcessed.Inc()
 }
 
+func checkPage(w http.ResponseWriter, r *http.Request) {
+	err := db.Ping()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = fmt.Fprintln(w, err.Error())
+		errorsProcessed.Inc()
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprintln(w, "ok")
+}
+
+func txPage(w http.ResponseWriter, r *http.Request) {
+	txProcessed.Inc()
+	_, err := db.Exec(query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = fmt.Fprintln(w, err.Error())
+		errorsProcessed.Inc()
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = fmt.Fprintln(w, "ok")
+}
+
 func main() {
+	var err error
+
 	color = os.Getenv("COLOR")
 	environment = os.Getenv("ENVIRONMENT")
+	listenAddress := os.Getenv("LISTEN_ADDRESS")
+	if listenAddress == "" {
+		listenAddress = ":5000"
+	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", rootPage)
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	query = os.Getenv("QUERY")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", rootPage)
+	router.HandleFunc("/tx", txPage)
+	router.HandleFunc("/.well-known/check", checkPage)
+	router.Handle("/metrics", promhttp.Handler())
+
 	// https://prometheus.io/docs/guides/go-application/
-	mux.Handle("/metrics", promhttp.Handler())
 
-	log.Println("Starting web server")
-	err := http.ListenAndServe(":5000", handlers.LoggingHandler(os.Stdout, mux))
+	log.Printf("Starting web server on %v", listenAddress)
+	err = http.ListenAndServe(listenAddress, handlers.LoggingHandler(os.Stdout, router))
 	if err != nil {
 		panic(err)
 	}
